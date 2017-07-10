78255a977cb470ac245f948a5f4110bd96acae0b
Maximilian Remming <maxremming@gmail.com>
2017-07-10 15:03:40 +0300

    updated to use sodexo "api"

--- index.js
+++ index.js
@@ -1,82 +1,111 @@
-var request = require("request-promise");
-var cheerio = require("cheerio");
-var moment = require("moment");
-var Slack = require("node-slack");
+const request = require("request-promise");
+const moment = require("moment");
+const IncomingWebhook = require("@slack/client").IncomingWebhook;
 
-if (process.env.SLACK_URL === undefined)
-  throw new Error("SLACK_URL environment variable is not set.");
-var slack = new Slack(process.env.SLACK_URL);
+function todaysMenu(restaurant_id, year, month, day) {
+  const now = moment();
 
-var URL = process.env.URL ||
-  "https://www.fi.issworld.com/palvelumme-service/ruokailupalvelut/ravintolat/midpoint-ruokalistasivu";
-var DATE_FORMAT = process.env.DATE_FORMAT || "dddd DD.MM.";
-var SLACK_USERNAME = process.env.SLACK_USERNAME || "Hungry Hungry Hippo";
-var SLACK_ICON_EMOJI = process.env.SLACK_ICON_EMOJI || ":fork_and_knife:";
+  restaurant_id = restaurant_id || process.env.RESTAURANT_ID;
+  year = year || now.format("YYYY");
+  month = month || now.format("MM");
+  day = day || now.format("DD");
 
-function getMenu() {
-  return request(URL, {
-    transform: data => {
-      return cheerio.load(data);
+  const url =
+    "http://www.sodexo.fi/ruokalistat/output/daily_json/" +
+    restaurant_id +
+    "/" +
+    year +
+    "/" +
+    month +
+    "/" +
+    day +
+    "/fi";
+
+  return request(url).then(data => {
+    data = JSON.parse(data);
+
+    const out = {
+      title: {},
+      categories: {}
+    };
+
+    for (i of data.courses) {
+      const cat = i.category.trim();
+
+      if (cat in out.categories) continue;
+
+      out.categories[cat] = [];
     }
-  }).then(data => {
-    var out = {};
-    var curDate = null;
 
-    data("table tbody tr td").each((i, el) => {
-      if (el.children.length > 0) {
-        if (el.children[0].data !== undefined) {
-          var data = el.children[0].data;
-          var d = moment(data, DATE_FORMAT);
+    out.title.text = data.meta.ref_title;
+    out.title.url = data.meta.ref_url;
 
-          if (d.isValid()) {
-            curDate = d;
-          } else if (curDate !== null) {
-            formattedDate = curDate.format(DATE_FORMAT);
-            if (out[formattedDate] === undefined) out[formattedDate] = [];
-            out[formattedDate].push(data);
-          }
-        }
+    for (i of data.courses) {
+      const course = {
+        title: {},
+        description: {}
+      };
+
+      for (k in i) {
+        if (i[k].length === 0) continue;
+        else if (k.startsWith("title_"))
+          course.title[k.substr("title_".length)] = i[k].trim();
+        else if (k.startsWith("desc_"))
+          course.description[k.substr("desc_".length)] = i[k].trim();
+        else if (k !== "category") course[k] = i[k].trim();
       }
-    });
+
+      out.categories[i.category.trim()].push(course);
+    }
+
     return out;
   });
 }
 
-function todaysMenu() {
-  return getMenu().then(data => {
-    var today = data[moment().format(DATE_FORMAT)];
+function slackFormat(data) {
+  const out = {
+    text: "", //"Here's todays menu.\nHave a great day! :sunny:",
+    attachments: []
+  };
 
-    if (today === undefined)
-      throw new Error("No menu for today!\n\n" + JSON.stringify(data));
-    return today;
+  out.attachments.push({
+    title: data.title.text,
+    title_link: data.title.url
   });
+
+  for (i in data.categories) {
+    const cat = {
+      title: i,
+      fields: []
+    };
+    for (k of data.categories[i]) {
+      cat.fields.push({
+        title:
+          k.title.fi +
+          (k.properties !== undefined ? " (" + k.properties + ")" : ""),
+        value: k.price,
+        short: true
+      });
+    }
+    out.attachments.push(cat);
+  }
+
+  return out;
 }
 
-todaysMenu()
-  .then(data => {
-    slack.send({
-      text: "Here's todays weather and menu.\nHave a great day! :metal:",
-      attachments: [
-        {
-          fallback: "Weather at Espoo right now.",
-          image_url: "http://wttr.in/Espoo.png?mp0&lang=en"
-        },
-        {
-          text: data.join("\n")
-        }
-      ],
-      username: SLACK_USERNAME,
-      icon_emoji: SLACK_ICON_EMOJI
-    });
+const slack_url = process.env.SLACK_URL;
+if (slack_url === undefined || slack_url.length === 0)
+  throw new Error("SLACK_URL environment variable is not set.");
+
+const slack = new IncomingWebhook(slack_url, {
+  username: process.env.SLACK_USERNAME || "Hungry Hungry Hippo",
+  iconEmoji: process.env.SLACK_ICON_EMOJI || ":fork_and_knife:"
+});
+
+todaysMenu().then(slackFormat).then(data =>
+  slack.send(data, err => {
+    if (err) throw Error(err);
   })
-  .catch(err => {
-    slack.send({
-      text: "Something went wrong when getting the menu :cold_sweat:.\n```" +
-        err +
-        "```",
-      username: SLACK_USERNAME,
-      icon_emoji: SLACK_ICON_EMOJI
-    });
-  });
+);
 
 exports.handler = todaysMenu;
